a6eea38edf20da8d87cb501dbb279df78a13ca17
garcia12340 <80352003+garcia12340@users.noreply.github.com>
2021-08-26 15:47:03 -0600

    Add files via upload

---
+++ ComponentesDeTexto/src/main/java/Main/Principal.java
@@ -0,0 +1,16 @@
+package Main;
+
+import principal.VentanaPrincipal;
+
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		VentanaPrincipal miVentanaPrincipal= new VentanaPrincipal();
+		miVentanaPrincipal.setVisible(true);
+	}
+
+}
