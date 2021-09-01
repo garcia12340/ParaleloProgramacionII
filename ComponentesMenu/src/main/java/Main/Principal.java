50b5139df00b089e34d32c223e6316bd6b0db81d
garcia12340 <80352003+garcia12340@users.noreply.github.com>
2021-09-01 15:47:04 -0600

    Add files via upload

---
+++ ComponentesMenu/src/main/java/Main/Principal.java
@@ -0,0 +1,17 @@
+package Main;
+
+import Principal.VentanaPrincipal;
+
+
+
+public class Principal {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	    VentanaPrincipal miFrame= new VentanaPrincipal();
+	    miFrame.setVisible(true);
+	}
+
+}
